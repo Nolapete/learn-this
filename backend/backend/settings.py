0aaec4db7a55bfc92e9c5d21057104dc8b6cd839
Pete LoGiudice <fish4pete@gmail.com>
2023-03-21 11:45:49 -0500

    remove SECRET_KEY

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from .base_settings import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--k*74+#$*-+_v)-61qq^&36q_wuxyxwdt0g@@lkcj5-#qa$$q)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
